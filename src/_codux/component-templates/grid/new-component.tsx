8e9242270bc8e491ffa874f65c23c8b625172ade
Diman353 <dmitryzozulya353@gmail.com>
2024-11-06 14:43:41 +0300

    Change default Codux Components settings

--- src/_codux/component-templates/grid/new-component.tsx
+++ src/_codux/component-templates/grid/new-component.tsx
@@ -5,9 +5,6 @@ export interface NewComponentProps {
     className?: string;
 }
 
-/**
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
 export const NewComponent = ({ className }: NewComponentProps) => {
     return (
         <div className={classNames(styles.root, className)}>
